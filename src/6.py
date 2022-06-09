8a83b2fce22489d430470c112e1f94ea72c424c6
0xpareto <pushpushX@protonmail.com>
2022-06-10 00:10:51 +1000

    Problem 6

---
+++ src/6.py
@@ -0,0 +1,14 @@
+"""
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+
+"""
+
+
+def main() -> int:
+    return sum([x for x in range(101)]) ** 2 - sum([x ** 2 for x in range(101)])
+
+
+if __name__ == "__main__":
+    result = main()
+    print(result)
